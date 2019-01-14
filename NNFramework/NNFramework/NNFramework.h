ca5fb7a2f9cb61b3925fb38578274abc8756b1ea
Nazifa Najish <nazifanajish@Admins-Mac-mini-3.local>
2019-01-14 13:16:59 +0530

    finished NN framework

---
+++ NNFramework/NNFramework/NNFramework.h
@@ -0,0 +1,19 @@
+//
+//  NNFramework.h
+//  NNFramework
+//
+//  Created by Nazifa Najish on 14/01/19.
+//  Copyright © 2019 Nazifa Najish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NNFramework.
+FOUNDATION_EXPORT double NNFrameworkVersionNumber;
+
+//! Project version string for NNFramework.
+FOUNDATION_EXPORT const unsigned char NNFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NNFramework/PublicHeader.h>
+
+
---
+++ NNFramework/NNFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  NNFramework
+//
+//  Created by Nazifa Najish on 14/01/19.
+//  Copyright © 2019 Nazifa Najish. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Here you Go...!"
+    }
+}
